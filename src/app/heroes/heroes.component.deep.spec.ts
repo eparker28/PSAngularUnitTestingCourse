eec660ec7c54fe31067700433cae07d561418ad0
Erik Parker <eparker28@hotmail.com>
2022-06-14 11:01:33 -0500

    Changes that completed the course

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -4,7 +4,20 @@ import {HeroService} from "../hero.service";
 import {of} from "rxjs";
 import {By} from "@angular/platform-browser";
 import {HeroComponent} from "../hero/hero.component";
-import {NO_ERRORS_SCHEMA} from "@angular/core";
+import { Directive, Input, NO_ERRORS_SCHEMA } from "@angular/core";
+
+@Directive({
+  selector: '[routerLink]',
+  host: { '(click)': 'onClick()' }
+})
+export class RouterLinkDirectiveStub {
+  @Input('routerLink') linkParams: any;
+  navigatedTo: any = null;
+
+  onClick() {
+    this.navigatedTo = this.linkParams;
+  }
+}
 
 describe('HeroesComponent (deep tests)', () => {
   let fixture: ComponentFixture<HeroesComponent>;
@@ -12,6 +25,8 @@ describe('HeroesComponent (deep tests)', () => {
   let HEROES;
 
   beforeEach(() => {
+
+    // A collection of heros for testing purposes
     HEROES = [
       {id: 1, name: 'SpiderDude', strength: 8},
       {id: 2, name: 'Wonderful Woman', strength: 24},
@@ -23,12 +38,13 @@ describe('HeroesComponent (deep tests)', () => {
     TestBed.configureTestingModule({
       declarations: [
         HeroesComponent,
-        HeroComponent
+        HeroComponent,
+        RouterLinkDirectiveStub
       ],
       providers: [
         { provide: HeroService, useValue: mockHeroService }
       ],
-      schemas: [NO_ERRORS_SCHEMA]
+      // schemas: [NO_ERRORS_SCHEMA]
     })
 
     fixture = TestBed.createComponent(HeroesComponent);
@@ -37,7 +53,6 @@ describe('HeroesComponent (deep tests)', () => {
   it('should render each hero as a HeroComponent', () => {
     mockHeroService.getHeroes.and.returnValue(of(HEROES));
 
-    // run ngOnInit
     fixture.detectChanges();
 
     const heroComponentsDEs = fixture.debugElement.queryAll(By.directive(HeroComponent));
@@ -45,5 +60,50 @@ describe('HeroesComponent (deep tests)', () => {
     for(let i=0; i < heroComponentsDEs.length; i++) {
       expect(heroComponentsDEs[i].componentInstance.hero).toEqual(HEROES[i]);
     }
-  })
+  });
+
+
+  it(`should call heroService.deleteHero when the Hero Component's
+    delete button is clicked`, () => {
+    spyOn(fixture.componentInstance, 'delete' );
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    // A test fixture that contains all HeroComponent instances
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    // (<HeroComponent>heroComponents[0].componentInstance).delete.emit(undefined);
+    heroComponents[0].triggerEventHandler('delete', null);
+
+      expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  });
+
+  it('should add a new hero to the hero list when the add button is clicked', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    const name = "Mr. Ice";
+    mockHeroService.addHero.and.returnValue(of({id: 5, name: name, strength: 4}));
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    const addButton = fixture.debugElement.queryAll(By.css('button'))[0];
+    inputElement.value = name;
+    addButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent
+    expect(heroText).toContain(name);
+  });
+
+  it('should have the correct route for the first hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+
+    let routerLink = heroComponents[0]
+      .query(By.directive(RouterLinkDirectiveStub))
+      .injector.get(RouterLinkDirectiveStub);
+
+    heroComponents[0].query(By.css('a')).triggerEventHandler('click', null);
+
+    expect(routerLink.navigatedTo).toBe('/detail/1');
+  });
+
 });
